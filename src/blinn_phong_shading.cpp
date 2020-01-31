452af85e146fb1b9caa6319faeed6aeec13a3b5f
LH <1550787615@qq.com>
2020-01-30 22:42:37 -0500

    upload

--- src/blinn_phong_shading.cpp
+++ src/blinn_phong_shading.cpp
@@ -22,33 +22,30 @@ Eigen::Vector3d blinn_phong_shading(
   Eigen::Vector3d rgb = Eigen::Vector3d(0,0,0);
   Eigen::Vector3d ia = Eigen::Vector3d(0.1,0.1,0.1);
 
-  Eigen::Vector3d ka, kd, ks;
-  double phong_exp;
+  Eigen::Vector3d ka = objects[hit_id]->material->ka;
+  Eigen::Vector3d kd = objects[hit_id]->material->kd;
+  Eigen::Vector3d ks = objects[hit_id]->material->ks;
+  double phong_exp = objects[hit_id]->material->phong_exponent;
+  rgb += (ka.array() * ia.array()).matrix();
 
-  Ray light_ray;
-  light_ray.origin = ray.origin + t * ray.direction;
+  Ray out_ray;
+  out_ray.origin = ray.origin + t * ray.direction;
   for (int i = 0; i < lights.size(); ++i)
   {
-    lights[i]->direction(light_ray.origin, light_ray.direction, max_light_t);
-    bool hit = first_hit(light_ray, epslion, objects, light_hit_id, light_t, light_n);
+    lights[i]->direction(out_ray.origin, out_ray.direction, max_light_t);
+    bool hit = first_hit(out_ray, epslion, objects, light_hit_id, light_t, light_n);
     if (!hit || light_t > max_light_t)
     {
-      ka = objects[hit_id]->material->ka;
-      kd = objects[hit_id]->material->kd;
-      ks = objects[hit_id]->material->ks;
-      phong_exp = objects[hit_id]->material->phong_exponent;
-
+      
       Eigen::Vector3d I = lights[i]->I;
-
-      Eigen::Vector3d h = (-ray.direction.normalized() + light_ray.direction.normalized()).normalized();
-      double tmp1 = std::max(h.dot(n), 0.0);
-      double tmp2 = std::max(n.dot(light_ray.direction), 0.0);
-      rgb += ((kd.array() * I.array()).matrix() * tmp2) + ((ks.array() * I.array()).matrix() * pow(tmp1, phong_exp));
+      Eigen::Vector3d h = (-ray.direction.normalized() + out_ray.direction.normalized()).normalized();
+     
+      double spec = std::max(h.dot(n), 0.0);
+      double diff = std::max(n.dot(out_ray.direction), 0.0);
+      rgb += ((kd.array() * I.array()).matrix() * diff) + ((ks.array() * I.array()).matrix() * pow(spec, phong_exp));
     }
   }
 
-  ka = objects[hit_id]->material->ka;
-  rgb += (ka.array() * ia.array()).matrix();
   return rgb;
 
   ////////////////////////////////////////////////////////////////////////////
