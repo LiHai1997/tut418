11f96d9fb66fb5b03efeb98df100e24b05c2084b
LH <1550787615@qq.com>
2020-01-30 21:12:24 -0500

    debug

--- src/blinn_phong_shading.cpp
+++ src/blinn_phong_shading.cpp
@@ -19,7 +19,7 @@ Eigen::Vector3d blinn_phong_shading(
     
   Eigen::Vector3d light_n;
   Eigen::Vector3d rgb = Eigen::Vector3d(0,0,0);
-  Eigen::Vector3d ia = Eigen::Vector3d(0.1,0.1,0.1)
+  Eigen::Vector3d ia = Eigen::Vector3d(0.1,0.1,0.1);
 
   Eigen::Vector3d ka = objects[hit_id]->material->ka;
   Eigen::Vector3d kd = objects[hit_id]->material->kd;
