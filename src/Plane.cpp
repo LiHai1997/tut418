503d5e56e6f710f066667d8cc6cb5c5b0f6e50a3
LH <1550787615@qq.com>
2020-01-30 22:13:29 -0500

    bug

--- src/Plane.cpp
+++ src/Plane.cpp
@@ -9,19 +9,40 @@ bool Plane::intersect(
   
 
   // ray is parallel to plane 
-  if (ray.direction.dot(normal) == 0)
-  {
-    return false;
-  }
+  // if (ray.direction.dot(normal) == 0)
+  // {
+  //   return false;
+  // }
 
-  double tmp = (point - ray.origin).dot(normal)/(ray.direction.dot(normal));
-  if (tmp >= min_t)
-  {
-    t = tmp;
-  	n = normal;
-  	return true;
-  }
-  return false;
+  // double tmp = (point - ray.origin).dot(normal)/(ray.direction.dot(normal));
+  // if (tmp >= min_t)
+  // {
+  //   t = tmp;
+  // 	n = normal;
+  // 	return true;
+  // }
+  // return false;
+  auto e = ray.origin, d = ray.direction, p = this->point, plane_normal = this->normal;
+
+    double numerator = (p - e).dot(plane_normal);
+    double denumerator = d.dot(plane_normal);
+    if (0 <= denumerator && denumerator<= 1e-6){
+        if (0 <= numerator && numerator <= 1e-6){
+            // ray lines in the plane
+            // not sure if this is the right way to handle this or not
+            t = min_t;
+            n = plane_normal;
+            return true;
+        }else // they never intersect
+            return false;
+    }
+    // They intersect, check if >= min_t
+    t = numerator / denumerator;
+    if (t >= min_t){
+        n = plane_normal;
+        return true;
+    }
+    return false;
   ////////////////////////////////////////////////////////////////////////////
 }
 
